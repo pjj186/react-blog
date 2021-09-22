f827d776d5b5f1c792051588648db4927a73fe98
Jong jae Park <pjj186@naver.com>
2021-09-22 21:03:07 +0900

    esm으로 ES모듈 import/export 사용
    esm 라이브러리를 설치하고, 프로젝트에 적용

--- src/api/posts/postsCtrl.js
+++ src/api/posts/postsCtrl.js
@@ -13,9 +13,8 @@ const posts = [
 POST /api/posts
 {title, body}
 */
-// export const write = (ctx) => {} 이거랑 같음
 
-exports.write = (ctx) => {
+export const write = (ctx) => {
   // REST API의 Request Body는 ctx.request.body에서 조회 할 수 있다.
   const { title, body } = ctx.request.body;
   postId += 1; // 기존 postId 값에 1을 더한다.
@@ -27,14 +26,14 @@ exports.write = (ctx) => {
 /* 포스트 목록 조회
 GET /api/posts
  */
-exports.list = (ctx) => {
+export const list = (ctx) => {
   ctx.body = posts;
 };
 
 /* 특정 포스트 조회
 GET /api/posts/:id
  */
-exports.read = (ctx) => {
+export const read = (ctx) => {
   const { id } = ctx.params;
   // 주어진 id 값으로 포스트를 찾는다.
   // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나
@@ -55,7 +54,7 @@ exports.read = (ctx) => {
 DELETE /api/posts/:id 
 */
 
-exports.remove = (ctx) => {
+export const remove = (ctx) => {
   const { id } = ctx.params;
   // 해당 id를 가진 post가 몇 번째인지 확인한다.
   const index = posts.findIndex((p) => p.id.toString() === id);
@@ -76,7 +75,7 @@ exports.remove = (ctx) => {
 PUT /api/posts/:id
 {title, body}
  */
-exports.replace = (ctx) => {
+export const replace = (ctx) => {
   // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용한다.
   const { id } = ctx.params;
   const { title, body } = ctx.request.body;
@@ -103,7 +102,7 @@ exports.replace = (ctx) => {
 PATCH /api/posts/:id
 { title, body }
 */
-exports.update = (ctx) => {
+export const update = (ctx) => {
   // PATCH 메서드는 주어진 필드만 교체한다.
   const { id } = ctx.params;
   // 해당 id를 가진 posts가 몇 번째인지 확인한다.
