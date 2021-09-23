ead7f79483a2541c9944a5f95c0812b69224d68c
Jong jae Park <pjj186@naver.com>
2021-09-23 18:50:20 +0900

    리스트 조회
    전체 리스트를 조회하는 코드

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -3,14 +3,6 @@ import * as postsCtrl from "./postsCtrl";
 
 const posts = new Router();
 
-const printInfo = (ctx) => {
-  ctx.body = {
-    method: ctx.method, // 현재 요청의 메서드
-    path: ctx.path, // 현재 요청의 경로
-    params: ctx.params, // 현재 요청의 파라미터
-  };
-};
-
 posts.get("/", postsCtrl.list);
 posts.post("/", postsCtrl.write);
 posts.get("/:id", postsCtrl.read);
--- src/api/posts/postsCtrl.js
+++ src/api/posts/postsCtrl.js
@@ -16,7 +16,16 @@ export const write = async (ctx) => {
   }
 };
 
-export const list = (ctx) => {};
+export const list = async (ctx) => {
+  // 데이터 조회
+  try {
+    const posts = await Post.find().exec();
+    // find() 함수를 호출한 후에는 exec()를 붙여 주어야 서버에 쿼리를 요청함!
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 export const read = (ctx) => {};
 
