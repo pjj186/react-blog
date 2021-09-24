f93fb464adf4bb991226e6c1ac6013743213491c
Jong jae Park <pjj186@naver.com>
2021-09-25 01:49:59 +0900

    가짜 데이터 삽입하기
    model.insertMany 메서드는 서버를 여러번 왕복하지않고 한번에 많은 양의 데이터를 삽입할 때 유용

---
+++ src/createFakeData.js
@@ -0,0 +1,16 @@
+import Post from "./models/post";
+
+export default function createFakeData() {
+  // 0, 1, ... 39로 이루어진 배열을 생성한 후 포스트 데이터로 변환
+  const posts = [...Array(40).keys()].map((i) => ({
+    title: `포스트 #${i}`,
+    // https://www.lipsum.com/에서 복사한 200자 이상의 텍스트
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    tags: ["가짜", "데이터"],
+  }));
+
+  // model.insertMany 메서드는 서버를 여러번 왕복하지않고 한번에 많은 양의 데이터를 삽입할 때 유용하다.
+  Post.insertMany(posts, (err, docs) => {
+    console.log(docs);
+  });
+}
