92887a6da23d06f820ef602f90660aa1e85890dd
Jong jae Park <pjj186@naver.com>
2021-09-25 18:16:21 +0900

    토큰 발급하기

--- src/api/auth/authCtrl.js
+++ src/api/auth/authCtrl.js
@@ -33,6 +33,12 @@ export const register = async (ctx) => {
 
     // 응답할 데이터에서 hashedPassword 필드 제거
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set("access_token", token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -62,6 +68,11 @@ export const login = async (ctx) => {
       return;
     }
     ctx.body = user.serialize();
+    const token = user.generateToken();
+    ctx.cookies.set("access_token", token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
